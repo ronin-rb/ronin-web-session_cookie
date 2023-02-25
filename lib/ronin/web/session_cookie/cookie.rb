759f0bb7a00b7cb03f68c4e35fa533ac0039edde
Postmodern <postmodern.mod3@gmail.com>
2023-02-24 19:57:10 -0800

    Forgot to add missing license headers.

--- lib/ronin/web/session_cookie/cookie.rb
+++ lib/ronin/web/session_cookie/cookie.rb
@@ -1,3 +1,21 @@
+# frozen_string_literal: true
+#
+# Copyright (c) 2023 Hal Brodigan (postmodern.mod3@gmail.com)
+#
+# ronin-web-session_cookie is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-web-session_cookie is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-web-session_cookie.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 module Ronin
   module Web
     module SessionCookie
